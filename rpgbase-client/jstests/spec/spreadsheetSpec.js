d53ed25b8691f4be75c4872683444336678f4f4c
Atul Varma <varmaa@gmail.com>
2016-01-24 10:05:20 -0500

    Add spreadsheet.js w/ test suite.

---
+++ rpgbase-client/jstests/spec/spreadsheetSpec.js
@@ -0,0 +1,49 @@
+describe("Spreadsheet", function() {
+  // Note that the following URL is NOT the same as the 'edit' link for
+  // the spreadsheet.
+  //
+  // For more details, see:
+  //
+  //   * http://labs.toolness.com/temp/gsheets_pub_to_web.png
+  //   * https://support.google.com/docs/answer/37579
+
+  var URL = "https://docs.google.com/spreadsheets/d/1EguVrU7noAf8a9BnHk6F7i-OMi4Q9Mh3aElm0e5N_ns/pubhtml";
+
+  it("should work", function() {
+    var sheet;
+    var spreadsheetLoaded = false;
+
+    runs(function() {
+      sheet = Spreadsheet.create(URL, function() {
+        spreadsheetLoaded = true;
+      });
+      expect(sheet.tabletopInfo).toBeNull();
+    });
+
+    waitsFor(function() {
+      return spreadsheetLoaded;
+    }, "The spreadsheet should load", 5000);
+
+    runs(function() {
+      expect(sheet.tabletopInfo).not.toBeNull();
+
+      expect(sheet.getWorksheetAsDicts('Foods')).toEqual([
+        {
+          "Food Name": "Hamburger",
+          "Energy": "3"
+        },
+        {
+          "Food Name": "Salad",
+          "Energy": "4"
+        }
+      ]);
+
+      expect(sheet.getWorksheetAsArrays('Foods')).toEqual([
+        // Note that we include the columns as the first row.
+        ['Food Name', 'Energy'],
+        ['Hamburger', '3'],
+        ['Salad', '4']
+      ]);
+    });
+  });
+});
